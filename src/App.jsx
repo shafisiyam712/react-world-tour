04ebcea4b969d6617219cb474e09e33c4297f19c
shafisiyam712 <shafisiyam674.com>
2024-10-20 04:08:39 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Countries from './components/Contries/Countries'
+function App() {
+
+  return (
+    <>
+      <h1>React World Tour</h1>
+      <Countries></Countries>
+      
+    </>
+  )
+}
+
+export default App
+//fearful-frogs.surge.sh
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,36 @@
+
+import { useState } from 'react';
+import './country.css'
+
+const Country = ({country , handleVisitedCountry, handleVisitedFlags}) => {
+    const{name,flags,population,area,cca3}=country
+
+    const[visited,setVisited]=useState(false)
+
+    const handleVisited=()=>{
+        setVisited(!visited)
+    }
+    
+    //console.log(handleVisitedCountry);//get the function
+    
+    return (
+        <div className={`country ? ${visited && 'visited'}`}>
+            <h3 style={{color:visited?'black':'white'}}>Name:{name.common}</h3>
+            <img src={flags.png} alt="" />
+            <p>Population:{population}</p>
+            <p>Area: {area}</p>
+            <small>Code: {cca3}</small>
+            <button onClick={()=>handleVisitedCountry(country)}>Mark as visited</button>
+            <br />
+            <button onClick={() => handleVisitedFlags(country.flags.png)}>Add Flag</button>
+            <br />
+            <button onClick={handleVisited}>{visited ? 'Visited' :'Ongoing'}</button>
+           {/* {visited && 'I have visited'} */}
+           {visited ? 'I have visited' :'I want to visit'}
+
+           
+        </div>
+    );
+};
+
+export default Country;
